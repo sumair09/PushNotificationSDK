6c09d8e5612c77409d38d8ae4c6a3f1042e54aaa
sumair yaseen <41383654+sumair09@users.noreply.github.com>
2025-05-19 14:52:28 +0500

    add callback to push notification sdk

--- Push Notification Library/src/main/java/com/pushnotificationlibrary/PushNotificationSDK.kt
+++ Push Notification Library/src/main/java/com/pushnotificationlibrary/PushNotificationSDK.kt
@@ -1,12 +1,14 @@
 package com.pushnotificationlibrary
 
 import android.content.Context
+import android.util.Log
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
+import com.google.firebase.messaging.FirebaseMessaging
 
 object PushNotificationSDK {
 
-    fun initialize(context: Context) {
+    fun initialize(context: Context, onTokenReceived: (String) -> Unit) {
         if (FirebaseApp.getApps(context).isEmpty()) {
             val options = FirebaseOptions.Builder()
                 .setApplicationId("1:93074134066:android:2a4baddf6c4e6c4659982b") // Replace with actual App ID
@@ -15,6 +17,16 @@ object PushNotificationSDK {
                 .build()
 
             FirebaseApp.initializeApp(context, options)
+            FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
+                if (!task.isSuccessful) {
+                    Log.e("PushNotificationSDK", "Fetching FCM token failed", task.exception)
+                    return@addOnCompleteListener
+                }
+                val token = task.result
+                Log.d("PushNotificationSDK", "FCM Token: $token")
+                onTokenReceived(token)
+            }
+
         }
     }
 }
