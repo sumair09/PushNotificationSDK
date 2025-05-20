6f31d8763c377e905b6a1c4740827c2339f41a17
sumair yaseen <41383654+sumair09@users.noreply.github.com>
2025-05-20 11:21:41 +0500

    make secret key dynamic

--- Push Notification Library/src/main/java/com/pushnotificationlibrary/PushNotificationSDK.kt
+++ Push Notification Library/src/main/java/com/pushnotificationlibrary/PushNotificationSDK.kt
@@ -8,25 +8,31 @@ import com.google.firebase.messaging.FirebaseMessaging
 
 object PushNotificationSDK {
 
-    fun initialize(context: Context, onTokenReceived: (String) -> Unit) {
+    fun initialize(
+        context: Context,
+        applicationId: String,
+        apiKey: String,
+        projectId: String,
+        onTokenReceived: (String) -> Unit
+    ) {
         if (FirebaseApp.getApps(context).isEmpty()) {
             val options = FirebaseOptions.Builder()
-                .setApplicationId("1:93074134066:android:2a4baddf6c4e6c4659982b") // Replace with actual App ID
-                .setApiKey("AIzaSyAAMV2GrsD4ghWK04oqFzgMUClUhHtMUAc")                            // Replace with actual API key
-                .setProjectId("push-notification-sdk-c8f7f")                 // Replace with actual Project ID
+                .setApplicationId(applicationId)
+                .setApiKey(apiKey)
+                .setProjectId(projectId)
                 .build()
 
             FirebaseApp.initializeApp(context, options)
-            FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
-                if (!task.isSuccessful) {
-                    Log.e("PushNotificationSDK", "Fetching FCM token failed", task.exception)
-                    return@addOnCompleteListener
-                }
-                val token = task.result
-                Log.d("PushNotificationSDK", "FCM Token: $token")
-                onTokenReceived(token)
-            }
+        }
 
+        FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.e("PushNotificationSDK", "Fetching FCM token failed", task.exception)
+                return@addOnCompleteListener
+            }
+            val token = task.result
+            Log.d("PushNotificationSDK", "FCM Token: $token")
+            onTokenReceived(token)
         }
     }
 }
